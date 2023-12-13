f609ef71b20e0fe672be61a457ab8c59f08101ae
jhkim <wellsy001012@gmail.com>
2023-12-13 23:16:14 +0900

    fix: HTTP exception 핸들링 수정

--- src/global/filter/exception.filter.ts
+++ src/global/filter/exception.filter.ts
@@ -2,15 +2,16 @@ import {
   ArgumentsHost,
   Catch,
   ExceptionFilter,
+  HttpException,
   HttpStatus,
   Logger,
+  UnauthorizedException,
 } from '@nestjs/common';
 import {
   BaseException,
   UnCatchedException,
 } from '../error/exceptions/base.exception';
 import { ValidationException } from '../error/exceptions/validation.exception';
-import { GlobalExceptionCodeEnum } from '../error/enums/exception-code.enum';
 
 // validation Exception 캐치
 @Catch(ValidationException)
@@ -36,30 +37,48 @@ export class ValidationExceptionFilter implements ExceptionFilter {
   }
 }
 
+@Catch(HttpException)
+export class HttpExceptionFilter implements ExceptionFilter {
+  logger = new Logger(HttpExceptionFilter.name);
+
+  catch(exception: HttpException, host: ArgumentsHost): void {
+    const ctx = host.switchToHttp(); // Exception Filter가 Http요청 외에도 동작하므로 필요
+    const request = ctx.getRequest();
+    const response = ctx.getResponse();
+
+    response.status(exception.getStatus()).json({
+      statusCode: exception.getStatus(),
+      message: exception.message,
+      timestamp: new Date(),
+      path: request.url,
+    });
+  }
+}
+
 // 전역 Exception 캐치
 @Catch()
 export class GlobalExceptionFilter implements ExceptionFilter {
   logger = new Logger(GlobalExceptionFilter.name);
 
   catch(exception: Error, host: ArgumentsHost): void {
-    this.logger.debug(exception);
+    console.log(exception);
 
     const ctx = host.switchToHttp(); // Exception Filter가 Http요청 외에도 동작하므로 필요
     const request = ctx.getRequest();
     const response = ctx.getResponse();
 
     const res =
-      exception instanceof BaseException
-        ? exception
-        : new UnCatchedException(exception.message);
+      exception instanceof BaseException ? exception : new UnCatchedException();
 
-    // Custom Error Response 정의
-    response.status(res.statusCode).json({
-      statusCode: res.statusCode,
-      errorCode: res.errorCode,
-      message: res.message,
-      timestamp: new Date(),
-      path: request.url,
-    });
+    if (exception instanceof BaseException) {
+      // Custom Error Response 정의
+      response.status(res.statusCode).json({
+        statusCode: res.statusCode,
+        errorCode: res.errorCode,
+        message: res.message,
+        timestamp: new Date(),
+        path: request.url,
+      });
+    }
   }
 }
