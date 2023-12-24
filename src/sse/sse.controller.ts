c3ec9b3568d0db5f2ca43457151f27586564faf9
jhkim <wellsy001012@gmail.com>
2023-12-24 23:29:54 +0900

    fix: api url 수정

--- src/sse/sse.controller.ts
+++ src/sse/sse.controller.ts
@@ -1,9 +1,31 @@
 import { Controller, MessageEvent, Sse } from '@nestjs/common';
 import { interval, map, Observable } from 'rxjs';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
-@Controller('sse')
+@ApiTags('SSE')
+@Controller('/api/v1/sse')
 export class SseController {
   // 10초에 한번씩 서버 시간 전송
+  @ApiOperation({
+    summary: 'SSE로 5초에 한번씩 서버 시간 unixTimestamp로 전송',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'OK',
+    content: {
+      'text/event-stream': {
+        schema: {
+          type: 'object',
+          properties: {
+            unixTime: {
+              example: 1627666800,
+              type: 'number',
+            },
+          },
+        },
+      },
+    },
+  })
   @Sse('time')
   sse(): Observable<MessageEvent> {
     return interval(5000).pipe(
